a94190b595077d6c8783fe82c2d1d5d1ff9be086
Ivan Steblenko <akudm2007@yandex.ru>
2018-04-19 14:34:24 +0400

    add text

--- app/components/hero/Hero.tsx
+++ app/components/hero/Hero.tsx
@@ -4,10 +4,12 @@ import { Title } from "../title/Title";
 import { IHeroProps } from "./IHeroProps";
 import { HeroRow } from "../hero-row/HeroRow";
 
+import './hero.module.scss';
+
 export class Hero extends React.Component<IHeroProps, null> {
     render() {
         return (
-            <div className="container">
+            <div className="container hero">
                 <Title value={this.props.title} />
                 {this.props.rows.map((row) => <HeroRow {...row} />)}
             </div>
